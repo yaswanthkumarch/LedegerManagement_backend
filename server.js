a11bd154e59cd4b4e91119d0a4a81f05d805df14
yaswanth <82691830+yaswanthkumarch@users.noreply.github.com>
2025-01-18 20:34:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
   "http://localhost:3000", // For local development
   "https://ledegermanagement-frontend.onrender.com", // For frontend on Render 
   
-  https://transaction-management-puce.vercel.app/, 
+  "https://transaction-management-puce.vercel.app/" , 
 
   "https://ledeger-management-frontend-a8lsnx7ko-yaswanthkumarchs-projects.vercel.app", // For frontend on Vercel
 ];
